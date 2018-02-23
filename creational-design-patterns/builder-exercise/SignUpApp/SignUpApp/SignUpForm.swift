73d6c03e3f18f5438b22322f86a675616c62a288
Chris Arriola <arriolac279@gmail.com>
2018-02-23 10:13:43 -0800

    Adding TODO statements.

---
+++ creational-design-patterns/builder-exercise/SignUpApp/SignUpApp/SignUpForm.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpForm.swift
+//  SignUpApp
+//
+//  Created by Christopher Arriola on 2/23/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+
+struct SignUpForm {
+    let email: String
+    let password: String
+    let username: String
+    
+    // TODO: Create builder class
+}
